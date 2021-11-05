de9f7bc3f5354404325ab2a2caa577f28b197e45
Nick Bennett <nick@nickbenn.com>
2021-11-05 12:25:16 -0600

    Implemented basic recyclerview of notes.

--- app/src/main/java/edu/cnm/deepdive/notekeeper/controller/EditNoteFragment.java
+++ app/src/main/java/edu/cnm/deepdive/notekeeper/controller/EditNoteFragment.java
@@ -1,6 +1,8 @@
 package edu.cnm.deepdive.notekeeper.controller;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -11,23 +13,36 @@ import androidx.lifecycle.ViewModelProvider;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import edu.cnm.deepdive.notekeeper.R;
 import edu.cnm.deepdive.notekeeper.databinding.FragmentEditNoteBinding;
+import edu.cnm.deepdive.notekeeper.model.entity.Note;
 import edu.cnm.deepdive.notekeeper.viewmodel.NoteViewModel;
 
-public class EditNoteFragment extends BottomSheetDialogFragment {
+public class EditNoteFragment extends BottomSheetDialogFragment implements TextWatcher {
 
   private FragmentEditNoteBinding binding;
   private NoteViewModel viewModel;
+  private long noteId;
+  private Note note;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    // TODO Get any parameter values passed in.
+    EditNoteFragmentArgs args = EditNoteFragmentArgs.fromBundle(getArguments());
+    noteId = args.getNoteId();
   }
 
   @Override
   public View onCreateView(
       @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     binding = FragmentEditNoteBinding.inflate(inflater, container, false);
+    binding.subject.addTextChangedListener(this);
+    binding.text.addTextChangedListener(this);
+    binding.cancel.setOnClickListener((v) -> dismiss());
+    binding.save.setOnClickListener((v) -> {
+      note.setSubject(binding.subject.getText().toString().trim());
+      note.setText(binding.text.getText().toString().trim());
+      viewModel.save(note);
+      dismiss();
+    });
     return binding.getRoot();
   }
 
@@ -35,12 +50,44 @@ public class EditNoteFragment extends BottomSheetDialogFragment {
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
     viewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+    if (noteId != 0) {
+      // TODO Set noteId in viewModel and observe viewModel.getNote().
+    } else {
+      note = new Note();
+    }
   }
 
   @Override
   public void onDestroyView() {
     super.onDestroyView();
     binding = null;
+  }
+
+  @Override
+  public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    // Do nothing.
+  }
+
+  @Override
+  public void onTextChanged(CharSequence s, int start, int before, int count) {
+    // Do nothing.
+  }
+
+  @Override
+  public void afterTextChanged(Editable s) {
+    checkSubmitConditions();
+  }
+
+  private void checkSubmitConditions() {
+    String subject = binding.subject
+        .getText()
+        .toString()
+        .trim();
+    String text = binding.text
+        .getText()
+        .toString()
+        .trim();
+    binding.save.setEnabled(!subject.isEmpty() && !text.isEmpty());
   }
 
 }
